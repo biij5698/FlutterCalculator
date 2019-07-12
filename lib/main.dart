6d973cc04a24cc19c5d7c3359b6b61091b20dc06
真地战士 <biij5698@sina.com>
2019-07-12 11:30:47 +0800

    the widget is builted

---
+++ lib/main.dart
@@ -0,0 +1,98 @@
+import 'package:flutter/material.dart';
+
+main() {
+  runApp(HomeWidget());
+}
+
+List<String> buttons = [
+  'c',
+  '+/-',
+  '%',
+  '÷',
+  '7',
+  '8',
+  '9',
+  'x',
+  '4',
+  '5',
+  '6',
+  '-',
+  '1',
+  '2',
+  '3',
+  '+',
+  '0',
+  '.',
+  'del',
+  '='
+];
+String result;
+
+class HomeWidget extends StatefulWidget {
+  @override
+  _HomeWidgetState createState() => _HomeWidgetState();
+}
+
+class _HomeWidgetState extends State<HomeWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Color(0xff403831),
+        body: SafeArea(
+          child: Column(
+            children: <Widget>[
+              Expanded(
+                child: Container(
+                  padding: EdgeInsets.only(bottom: 10.0, right: 10.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: <Widget>[
+                      Text(
+                        result ??= "",
+                        style:
+                            TextStyle(color: Color(0xffFFFFFF), fontSize: 32.0),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+              GridView.count(
+                crossAxisCount: 4,
+                shrinkWrap: true,
+                crossAxisSpacing: 2.0,
+                mainAxisSpacing: 2.0,
+                children: List.generate(buttons.length, (index) {
+                  return FlatButton(
+                    child: buttons[index] == 'del'
+                        ? IconButton(
+                            // icon: ImageIcon(AssetImage('images/del.png')),
+                            icon: Image.asset('images/del.png'),
+                          )
+                        : Text(
+                            buttons[index],
+                            style: TextStyle(
+                                color: Color(0xffE9E8E7),
+                                fontSize: 32.0,
+                                fontWeight: FontWeight.w100),
+                          ),
+                    onPressed: () {
+                      setState(() {
+                        result = buttons[index];
+                      });
+                    },
+                    color: Color(0xff706A63),
+                    splashColor: Colors.transparent,
+                    highlightColor: Color(0xffA8A4A1),
+                  );
+                }).toList(),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
